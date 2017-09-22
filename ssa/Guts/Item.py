12839095667c5fe0b4b64490445d4a4e96230661
Alex Palacio <alex.palacio9@gmail.com>
2017-09-22 00:15:08 -0500

    item is pretty much done.
    Subclasses are done. Clothing, toys, electronics, household items, and books.

--- ssa/Guts/Item.py
+++ ssa/Guts/Item.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from ssa.Guts.db import conn, sqlite_file
+from ssa.Guts.db import sqlite_file
 
 
 class Item:
@@ -10,27 +10,32 @@ class Item:
         self.itemQuantity = quantity
         self.itemName = ''
         self.itemDescription = ''
-        self.category = ''
-        self.conn = sqlite3.connect(sqlite_file)
-        self.cursor = conn.cursor()
+        self.itemCategory = ''
+        self.getItem()
 
     def getItem(self):
-        self.cursor.execute('SELECT price, quantity, name, description FROM INVENTORY WHERE ID = ', self.itemId, ';')
-        # Get the
-        row = self.cursor.fetchone()
+        conn = sqlite3.connect(sqlite_file)
+        cursor = conn.cursor()
+        cursor.execute("SELECT price, quantity, name, description FROM INVENTORY WHERE ID = '%s' " % self.itemID)
+        # Get the item info from the database.
+        row = cursor.fetchone()
         self.itemCost = row[0]
         self.itemQuantity = row[1]
         self.itemName = row[2]
         self.itemDescription = row[3]
+        conn.close()
 
-    def getItemName(self):
-        return self.itemName
-
-    def getItemCost(self):
-        return self.itemCost
-
-    def changeItemQuantity(self, newQuantity):
-        self.cursor.execute('update inventory set item_quan=%s where item_id = %s', (newQuantity, self.itemName))
+    def changeItemQuantity(self):
+        conn = sqlite3.connect(sqlite_file)
+        cursor = conn.cursor()
+        # Get current quantity
+        cursor.execute("Select quantity FROM INVENTORY WHERE ID = '%s'" % self.itemID)
+        result = cursor.fetchone()
+        newQuantity = result[0] - self.itemQuantity
+        # update quantity in DB, commit, and close connection
+        cursor.execute('update inventory set quantity=%s where ID = %s', (newQuantity, self.itemID))
+        conn.commit()
+        conn.close()
 
 
 class Toys (Item):
@@ -38,15 +43,34 @@ class Toys (Item):
         super().__init__(itemID, quantity)
         self.isActionFigure = False
         self.ageRange = ''
-        self.category = 'toy'
+        self.itemCategory = 'Toy'
+
+        conn = sqlite3.connect(sqlite_file)
+        cursor = conn.cursor()
+        cursor.execute("SELECT age, isActionFigure FROM TOYS_INVENTORY WHERE ID = '%s' " % self.itemID)
+        result = cursor.fetchone()
+
+        self.ageRange = result[0]
+        self.isActionFigure = result[1]
+
+        conn.close()
 
 
 class Book (Item):
     def __init__(self, itemID, quantity):
         super().__init__(itemID, quantity)
-        self.isbn = ''
+        self.isbn = 0
         self.author = ''
-        self.category = 'book'
+        self.itemCategory = 'Book'
+
+        conn = sqlite3.connect(sqlite_file)
+        cursor = conn.cursor()
+        cursor.execute("SELECT ISBN, author FROM BOOKS_TABLE WHERE ID = '%s' " % self.itemID)
+        result = cursor.fetchone()
+
+        self.isbn = result[0]
+        self.author = result[1]
+        conn.close()
 
 
 class Household (Item):
@@ -54,7 +78,16 @@ class Household (Item):
         super().__init__(itemID, quantity)
         self.room = ''
         self.isLuxuryItem = False
-        self.category = 'household item'
+        self.itemCategory = 'Household item'
+
+        conn = sqlite3.connect(sqlite_file)
+        cursor = conn.cursor()
+        cursor.execute("SELECT room, isluxuryitem FROM HH_INVENTORY WHERE ID = '%s' " % self.itemID)
+        result = cursor.fetchone()
+
+        self.room = result[0]
+        self.isLuxuryItem = result[1]
+        conn.close()
 
 
 class Electronic (Item):
@@ -62,4 +95,30 @@ class Electronic (Item):
         super().__init__(itemID, quantity)
         self.brand = ''
         self.category = ''
-        self.category = 'electronic'
+        self.itemCategory = 'electronic'
+
+        conn = sqlite3.connect(sqlite_file)
+        cursor = conn.cursor()
+        cursor.execute("SELECT brand, category FROM ELEC_INVENTORY WHERE ID = '%s' " % self.itemID)
+        result = cursor.fetchone()
+
+        self.brand = result[0]
+        self.category = result[1]
+        conn.close()
+
+
+class Clothes (Item):
+    def __init__(self,itemID, quantity):
+        super().__init__(itemID, quantity)
+        self.gender = ''
+        self.section = ''
+        self.itemCategory = 'clothing'
+
+        conn = sqlite3.connect(sqlite_file)
+        cursor = conn.cursor()
+        cursor.execute("SELECT gender, section FROM ELEC_INVENTORY WHERE ID = '%s' " % self.itemID)
+        result = cursor.fetchone()
+
+        self.gender = result[0]
+        self.section = result[1]
+        conn.close()
