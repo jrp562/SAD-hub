12edc89409bf0aadde7137257755b70e1654733e
Alex Palacio <alex.palacio9@gmail.com>
2017-09-21 21:39:00 -0500

    Subclasses introduced
    Created subclasses. Need to write the database queries to get the proper info

--- ssa/Guts/Item.py
+++ ssa/Guts/Item.py
@@ -14,8 +14,8 @@ class Item:
         self.conn = sqlite3.connect(sqlite_file)
         self.cursor = conn.cursor()
 
-    def GetItem(self):
-        self.cursor.execute('select item_id, item_cost, item_quan, item_name, item_desc from inventory')
+    def getItem(self):
+        data = self.cursor.execute('select item_id, item_cost, item_quan, item_name, item_desc from inventory')
         row = self.cursor.fetchone()
         for row in data:
             self.itemId = row[0]
@@ -24,14 +24,42 @@ class Item:
             self.itemName = row[3]
             self.itemDescription = row[4]
 
-    def GetItemName(self):
+    def getItemName(self):
         return self.itemName
 
-    def GetItemCost(self):
+    def getItemCost(self):
         return self.itemCost
 
-    def ChangeItemQuantity(self, newQuantity):
+    def changeItemQuantity(self, newQuantity):
         self.cursor.execute('update inventory set item_quan=%s where item_id = %s', (newQuantity, self.itemName))
 
-    def ChangeItemCost(self, newCost):
-        self.ItemCost = newCost
+    def changeItemCost(self, newCost):
+        self.itemCost = newCost
+
+
+class Toys (Item):
+    def __init__(self):
+        super().__init__()
+        self.isActionFigure = False
+        self.ageRange = ''
+
+
+class Book (Item):
+    def __init__(self):
+        super().__init__()
+        self.isbn = ''
+        self.author = ''
+
+
+class Household (Item):
+    def __init__(self):
+        super().__init__()
+        self.room = ''
+        self.isLuxuryItem = False
+
+
+class Electronic (Item):
+    def __init__(self):
+        super().__init__()
+        self.brand = ''
+        self.category = ''
