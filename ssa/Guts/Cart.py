8814cb283088cfedbfe7e6756ba4772b863f5140
FlaxenTrousers <langolier009@gmail.com>
2017-09-21 23:41:35 -0500

    Updated Cart class
    Overhauled the Cart class, purchaseCart method not completely finished yet.

--- ssa/Guts/Cart.py
+++ ssa/Guts/Cart.py
@@ -1,30 +1,56 @@
 import os
 from ssa.Guts.Item import Item
 
+
 class Cart:
     def __init__(self, user):
-        self.cartid = ''
+        self.cartID = ''
         self.user = user
         self.cartItems = []
 
-    def showItems(self):
-        #get cart items from db and insert into list
-        pass
+    def showItems(self, cartItems):
+        print(*cartItems, sep='\n')
 
     def purchaseCart(self):
-        #prompt for address/creditcard
-        #prompt for confirmation
-        #put info and items into db
-        pass
+        while 1:
+            if self.user.status == "not_logged_in":
+                # prompt for address/credit card if not logged in
+                address = input("Please enter your address for shipping purposes: \n")
+                creditCard = input("Please provide your credit card number: \n")
+                print("Shipping Address: \n", address, "\nCredit Card Number: \n", creditCard)
+                # prompt for confirmation
+                confirm = input("Is the information displayed correct? Y/N\n")
+                if confirm == 'Y' or confirm == 'y':
+                    finalConfirm = input("Would you like to confirm this purchase? Y/N\n")
+                    if finalConfirm == 'y' or finalConfirm == 'Y':
+                        # put info and items in db
+                        break
+            elif self.user.status == "logged_in":
+                # print address/credit card and ask if information displayed is correct
+                print("Shipping Address: \n")
+                self.user.printUserAddress()
+                print("Credit Card Number: \n", self.user.cardNumber)
+                # prompt for confirmation
+                confirm = input("Is the information displayed correct? Y/N\n")
+                if confirm == 'Y' or confirm == 'y':
+                    finalConfirm = input("Would you like to confirm this purchase? Y/N\n")
+                    if finalConfirm == 'y' or finalConfirm == 'Y':
+                        # put info and items in db
+                        break
+
 
     def itemTotal(self):
         total = 0.0
-        for each in cartItems:
+        for each in self.cartItems:
             total += each.itemCost * each.itemQuantity
-            print("$", total)
 
-    def addItem(self, itemID):
-        pass
+        print("$", total)
+
+    def addItem(self, itemID, quantity):
+        newItem = Item(itemID, quantity)
+        self.cartItems.append(newItem)
 
     def removeItem(self, itemID):
-        pass
+        for each in self.cartItems:
+            if each.itemID == itemID:
+                del self.cartItems[each]
