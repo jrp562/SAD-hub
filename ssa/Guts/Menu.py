1263b1cdde248eac99d84b4a4c8301f269b68aaa
jwb623 <jwb623@gmail.com>
2017-09-21 21:02:41 -0500

    more functions in User and some bug fixes in Menu

--- ssa/Guts/Menu.py
+++ ssa/Guts/Menu.py
@@ -4,25 +4,25 @@ from ssa.Guts.Cart import Cart
 
 def Menu(user, status, cart):
     if (status == 'not_logged_in'):
-        selection = 0
+        selection = '0'
         print()
         print("*********Super Store App***********")
         print("1. Login")
         print("2. View Products")
         print("3. View Cart")
         print("4. Exit")
-        selection = int(input("Please make a selection: "))
+        selection = input("Please make a selection: ")
 
 
-        if selection == 1:
+        if selection == '1':
             uname = input("Username: ")
             pword = input("Password: ")
             user.logIn(uname, pword)
             os.system('cls' if os.name == 'nt' else 'clear')
             selection = Menu(user, user.status, cart)
 
-        elif selection == 2:
-            #access db to get itemList
+        elif selection == '2':
+            #access db to get itemList or use cart class function to return the itemlist
             os.system('cls' if os.name == 'nt' else 'clear')
             print ('*********** Our Products ************')
             itemList = ['Boots', 'Grapefruit', 'Headphones', 'How to Use a Hammer by Pinky', 'Atlas', 'Shingles', 'Football', 'Great Expectations']
@@ -35,17 +35,17 @@ def Menu(user, status, cart):
             print("1. Add an item to cart.")
             print("2. View Details.")
             print("3. Go Back.")
-            selection = int(input("Please make a selection: "))
+            selection = input("Please make a selection: ")
 
-            if selection == 1:
+            if selection == '1':
                 # do crazy shit
                 pass
 
-            elif selection == 2:
+            elif selection == '2':
                 #cart.purchaseCart()
                 pass
 
-            elif selection == 3:
+            elif selection == '3':
                 os.system('cls' if os.name == 'nt' else 'clear')
                 selection = Menu(user, user.status, cart)
 
@@ -53,7 +53,7 @@ def Menu(user, status, cart):
                 erroneousInput(user, cart)
 
 
-        elif selection == 3:
+        elif selection == '3':
             #get data from cart and add to list
             os.system('cls' if os.name == 'nt' else 'clear')
             print ('*********** Your Cart ************')
@@ -67,24 +67,24 @@ def Menu(user, status, cart):
             print("1. Remove an item from cart.")
             print("2. Purchase cart.")
             print("3. Go Back.")
-            selection = int(input("Please make a selection: "))
+            selection = input("Please make a selection: ")
 
-            if selection == 1:
+            if selection == '1':
                 # do crazy shit
                 pass
 
-            elif selection == 2:
+            elif selection == '2':
                 #purchaseCart()
                 pass
 
-            elif selection == 3:
+            elif selection == '3':
                 os.system('cls' if os.name == 'nt' else 'clear')
                 selection = Menu(user, user.status, cart)
 
             else:
                 erroneousInput(user, cart)
 
-        elif selection == 4:
+        elif selection == '4':
             os.system('cls' if os.name == 'nt' else 'clear')
             exit()
 
@@ -94,20 +94,20 @@ def Menu(user, status, cart):
         return selection
 
     if (status == 'logged_in'):
-        selection = 0
+        selection = '0'
         print()
         print("*********Super Store App***********")
         print("1. View Purchase History")
         print("2. View Products")
         print("3. View Cart")
         print("4. Log Off / Exit")
-        selection = int(input("Please make a selection: "))
+        selection = input("Please make a selection: ")
 
-        if selection == 1:
+        if selection == '1':
             user.getPurchaseHistory()
             selection = Menu(user, user.status, cart)
 
-        elif selection == 2:
+        elif selection == '2':
             #access db to get itemList
             os.system('cls' if os.name == 'nt' else 'clear')
             print ('*********** Our Products ************')
@@ -120,20 +120,20 @@ def Menu(user, status, cart):
             print ('*************** Options *****************')
             print("1. Add an item to cart.")
             print("2. Go Back.")
-            selection = int(input("Please make a selection: "))
+            selection = input("Please make a selection: ")
 
-            if selection == 1:
+            if selection == '1':
                 # do crazy shit
                 pass
 
-            elif selection == 2:
+            elif selection == '2':
                 os.system('cls' if os.name == 'nt' else 'clear')
                 selection = Menu(user, user.status, cart)
 
             else:
                 erroneousInput(user, cart)
 
-        elif selection == 3:
+        elif selection == '3':
             os.system('cls' if os.name == 'nt' else 'clear')
             print ('***********' + user.username + ' Cart ************')
             itemList = ['Boots', 'Grapefruit', 'Headphones', 'How to Use a Hammer by Pinky']
@@ -146,24 +146,24 @@ def Menu(user, status, cart):
             print("1. Remove an item from cart.")
             print("2. Purchase cart.")
             print("3. Go Back.")
-            selection = int(input("Please make a selection: "))
+            selection = input("Please make a selection: ")
 
-            if selection == 1:
+            if selection == '1':
                 # do crazy shit
                 pass
 
-            elif selection == 2:
+            elif selection == '2':
                 #purchaseCart()
                 pass
 
-            elif selection == 3:
+            elif selection == '3':
                 os.system('cls' if os.name == 'nt' else 'clear')
                 selection = Menu(user, user.status, cart)
 
             else:
                 erroneousInput(user, cart)
 
-        elif selection == 4:
+        elif selection == '4':
             os.system('cls' if os.name == 'nt' else 'clear')
             exit()
 
--- ssa/Guts/User.py
+++ ssa/Guts/User.py
@@ -20,6 +20,7 @@ class User:
         data = c.fetchall()
         # print (data)
     # check password and set variables if correct
+    # TODO: needs to be updated when db is updated
         if (data[0][2] == password):
             self.status = 'logged_in'
             self.userID = data[0][0]
@@ -36,7 +37,7 @@ class User:
         self.status = 'not_logged_in'
 
     def getPurchaseHistory(self):
-        # access db for a list of items adn dates then put into itemList
+    # TODO: access db for a list of items and dates then put into itemList
         os.system('cls' if os.name == 'nt' else 'clear')
         print('***********' + self.username + ' Purchase History ************')
         itemList = ['Hammer', 'Grapefruit',
@@ -91,7 +92,12 @@ class User:
         address[1] = input("Enter the city: ")
         address[2] = input("Enter the state: ")
         address[3] = int(input("Enter your zipcode: "))
-        # create insert statement for db
+        sqlite_file = 'shop_db.db'
+        conn = sqlite3.connect(sqlite_file)
+        c = conn.cursor()
+        c.execute("INSERT INTO USERS (ADDRLINE1, CITY, STATE, ZIP) VALUES ({a1},{a2},{a3},{a4})".\
+                format(a1=address[0], a2=address[1], a3=address[2], a4=address[3]))
+        conn.close()
 
     def setUserCCNUM(self):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -102,11 +108,16 @@ class User:
             print(' Invalid Card Number! ')
             return False
         else:
-            print("Would like us to save your Card Number? (y/n): ")
+            print("Would you like us to save your Card Number? (y/n): ")
             answer = input()
             if (answer == 'y'):
-                # insert into db
-                pass
+                sqlite_file = 'shop_db.db'
+                conn = sqlite3.connect(sqlite_file)
+                c = conn.cursor()
+                c.execute("INSERT INTO USERS (CCNUM) VALUES ({a1})".\
+                        format(a1=self.creditCardNumber))
+                conn.close()
+                return True
             elif (answer == 'n'):
                 print("Card number not saved.")
                 return True
