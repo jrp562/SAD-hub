b7d3abb3c9e73c8bdf3eaceb13ab5cc42ade3a6f
Alex Palacio <alex.palacio9@gmail.com>
2017-09-21 20:40:39 -0500

    Item Updates

--- ssa/Guts/Item.py
+++ ssa/Guts/Item.py
@@ -1,5 +1,8 @@
 import sqlite3
 
+from ssa.Guts.db import conn, sqlite_file
+
+
 class Item:
     def __init__(self):
         self.itemId = ''
@@ -9,27 +12,26 @@ class Item:
         self.itemDescription = ''
         self.category = ''
         self.conn = sqlite3.connect(sqlite_file)
-        cursor = conn.cursor()
+        self.cursor = conn.cursor()
 
     def GetItem(self):
-    	cursor.execute('select item_id, item_cost, item_quan, item_name, item_desc from inventory')
-    	row = cursor.fetchone()
-    	for row in data:
-    		self.itemId = row[0]
-    		self.itemCost = row[1]
-    		self.itemQuantity = row[2]
-    		self.itemName = row[3]
-    		self.itemDescription = row[4]
-
+        self.cursor.execute('select item_id, item_cost, item_quan, item_name, item_desc from inventory')
+        row = self.cursor.fetchone()
+        for row in data:
+            self.itemId = row[0]
+            self.itemCost = row[1]
+            self.itemQuantity = row[2]
+            self.itemName = row[3]
+            self.itemDescription = row[4]
 
     def GetItemName(self):
-    	return self.itemName
+        return self.itemName
 
     def GetItemCost(self):
         return self.itemCost
 
     def ChangeItemQuantity(self, newQuantity):
-        cursor.execute('update inventory set item_quan=%s where item_id = %s', (newQuantity, self.itemName))
+        self.cursor.execute('update inventory set item_quan=%s where item_id = %s', (newQuantity, self.itemName))
 
-    def ChangeItemCost(self):
+    def ChangeItemCost(self, newCost):
         self.ItemCost = newCost
